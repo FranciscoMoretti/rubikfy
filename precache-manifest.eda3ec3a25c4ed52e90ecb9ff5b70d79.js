b56438af1bce09fc6d295192b96c31d15a5fd0ee
Francisco Moretti <franciscoemoretti@gmail.com>
2020-01-18 10:30:54 -0300

    Updates

---
+++ precache-manifest.eda3ec3a25c4ed52e90ecb9ff5b70d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b724eecc0be4e5082e961299422683ec",
+    "url": "/rubikfy/index.html"
+  },
+  {
+    "revision": "f3250829d27f053bde31",
+    "url": "/rubikfy/static/css/main.328dbb43.chunk.css"
+  },
+  {
+    "revision": "399a28900b617875610d",
+    "url": "/rubikfy/static/js/2.c882f6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rubikfy/static/js/2.c882f6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3250829d27f053bde31",
+    "url": "/rubikfy/static/js/main.30a44d09.chunk.js"
+  },
+  {
+    "revision": "e161a65126a4e24f30be",
+    "url": "/rubikfy/static/js/runtime-main.f8c3f59a.js"
+  }
+]);
