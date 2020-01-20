05d919cd35271e7bc54181d203f587f042c18689
Francisco Moretti <franciscoemoretti@gmail.com>
2020-01-19 22:08:55 -0300

    Updates

--- precache-manifest.187f2169dede71c95aa30f4ca362bb78.js
+++ precache-manifest.187f2169dede71c95aa30f4ca362bb78.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95dc239ff3ee3c09459f31c3dfe0932a",
+    "revision": "bb19fb252f2e0eaa089227ca838b61e5",
     "url": "/rubikfy/index.html"
   },
   {
-    "revision": "137ea71704e1474db955",
+    "revision": "2bcb824993b8f23d7e75",
     "url": "/rubikfy/static/css/2.c719baa7.chunk.css"
   },
   {
-    "revision": "c703c37e68a435440a65",
+    "revision": "832721ef39fafe66fa2d",
     "url": "/rubikfy/static/css/main.28d40738.chunk.css"
   },
   {
-    "revision": "137ea71704e1474db955",
-    "url": "/rubikfy/static/js/2.761f2976.chunk.js"
+    "revision": "2bcb824993b8f23d7e75",
+    "url": "/rubikfy/static/js/2.65102a9c.chunk.js"
   },
   {
     "revision": "c9cd5feff121088a4bdef3d5b8034ae1",
-    "url": "/rubikfy/static/js/2.761f2976.chunk.js.LICENSE"
+    "url": "/rubikfy/static/js/2.65102a9c.chunk.js.LICENSE"
   },
   {
-    "revision": "c703c37e68a435440a65",
-    "url": "/rubikfy/static/js/main.bae960bf.chunk.js"
+    "revision": "832721ef39fafe66fa2d",
+    "url": "/rubikfy/static/js/main.2a52789c.chunk.js"
   },
   {
     "revision": "e161a65126a4e24f30be",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rubikfy/precache-manifest.7ae428fd2269fe8534cad1bff6298f4c.js"
+  "/rubikfy/precache-manifest.187f2169dede71c95aa30f4ca362bb78.js"
 );
 
 self.addEventListener('message', (event) => {
