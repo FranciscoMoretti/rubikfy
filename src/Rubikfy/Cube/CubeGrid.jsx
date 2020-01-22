3f7810b9b5d775bfddc3e365ec9f10aa7e2e4edd
Francisco Moretti <franciscoemoretti@gmail.com>
2020-01-21 21:59:14 -0300

    Resize component height and width dynamically

--- src/Rubikfy/Cube/CubeGrid.jsx
+++ src/Rubikfy/Cube/CubeGrid.jsx
@@ -4,10 +4,24 @@ import Cube from './Cube';
 import './CubeGrid.css';
 
 export default class CubeGrid extends Component {
+    adaptWidthHeight() {
+        if (this.props.grid_width > this.props.grid_height) {
+            return {
+                height: `${30 * (this.props.grid_height / this.props.grid_width)}vw`,
+                width: `${30}vw`,
+            };
+        } else {
+            return {
+                height: `${30}vw`,
+                width: `${30 * (this.props.grid_width / this.props.grid_height)}vw`,
+            };
+        }
+    }
+
     render() {
         return (
             <div className="grid"
-                style={{ height: `${50 * (this.props.grid_height / this.props.grid_width)}vw` }}
+                style={this.adaptWidthHeight()}
                 onMouseUp={() => this.props.onMouseUp()}
             >
                 {this.props.grid.map((c_row, c_rowIdx) => {
