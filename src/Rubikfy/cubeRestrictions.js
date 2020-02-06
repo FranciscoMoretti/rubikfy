a476cdf3aeef0c9e3821aaeb231f263d5c33301c
Francisco Moretti <franciscoemoretti@gmail.com>
2020-02-06 20:28:24 -0300

    Create class to manage cubelets relations

---
+++ src/Rubikfy/cubeRestrictions.js
@@ -0,0 +1,19 @@
+export default class CubeRestrictions {
+    // 26 Cubelets (6 centers + 8 corners + 12 edges)
+    centerColor = ['U', 'R', 'F', 'D', 'L', 'B'];
+
+    cornerColor = [['U', 'R', 'F'], ['U', 'F', 'L'], ['U', 'L', 'B'], ['U', 'B', 'R'], ['D', 'F', 'R'], ['D', 'L', 'F'], ['D', 'B', 'L'], ['D', 'R', 'B']];
+
+    edgeColor = [['U', 'R'], ['U', 'F'], ['U', 'L'], ['U', 'B'], ['D', 'R'], ['D', 'F'], ['D', 'L'], ['D', 'B'], ['F', 'R'], ['F', 'L'], ['B', 'L'], ['B', 'R']];
+
+    // Centers relative position is fixed
+    getOpositeCenter(center) {
+        var ind = this.centerColor.indexOf(center);
+        if (ind === -1) {
+            return ""
+        } else {
+            return this.centerColor[(ind + 3) % 6];
+        }
+    }
+
+}
