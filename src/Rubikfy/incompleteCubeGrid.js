a7a8d46c581d3c6a8e4740b3deface3bd9bf228b
Francisco Moretti <franciscoemoretti@gmail.com>
2020-02-08 17:43:12 -0300

    Create IncompleteCubeGrid class

---
+++ src/Rubikfy/incompleteCubeGrid.js
@@ -0,0 +1,45 @@
+import { reshapeArrayToCubeFaceGrid } from './Cube/CubeGrid'
+import { IncompleteCube } from './incompleteCube'
+
+export class IncompleteCubeGrid {
+
+    constructor(width, height) {
+        this.grid = this.getInitialCubeGrid(width, height);
+        this.width = width;
+        this.height = height;
+    }
+
+
+    getInitialCubeGrid(width, height) {
+        const grid = [];
+        for (let row = 0; row < height; row++) {
+            const currentRow = [];
+            for (let col = 0; col < width; col++) {
+                currentRow.push(new IncompleteCube());
+            }
+            grid.push(currentRow);
+        }
+        return grid;
+    };
+
+    toFaceGrid(face) {
+        const faceGrid = [];
+        for (let row = 0; row < this.height; row++) {
+            const currentRow = [];
+            for (let col = 0; col < this.width; col++) {
+                currentRow.push(this.grid[row][col].getFaceRGBMatrix(face));
+            }
+            faceGrid.push(currentRow);
+        }
+        return faceGrid;
+    }
+
+    setFaceGrid(face, faceGrid) {
+        for (let row = 0; row < this.height; row++) {
+            for (let col = 0; col < this.width; col++) {
+                this.grid[row][col].setFaceRGBColor(face, faceGrid[row][col]);
+            }
+        }
+    }
+
+}
