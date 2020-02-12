db5644d2ff141d3c6ba56469fdc76deeae22ca6f
Francisco Moretti <franciscoemoretti@gmail.com>
2020-02-11 22:40:54 -0300

    create function randomizeSideFaces based on Cube
    The function should eventually be able to create a complete solvable
    cube starting from two defined opossing faces

--- src/Rubikfy/incompleteCube.js
+++ src/Rubikfy/incompleteCube.js
@@ -1,4 +1,5 @@
 import CubeQuantization from './cubeQuantization'
+import Cube, { solve } from "cubejs"
 
 const RUBIK_COLORS = {
     U: { r: 255, g: 255, b: 255 },  //white
@@ -32,6 +33,117 @@ cornerColor = [['U', 'R', 'F'], ['U', 'F', 'L'], ['U', 'L', 'B'], ['U', 'B', 'R'
 
 edgeColor = [['U', 'R'], ['U', 'F'], ['U', 'L'], ['U', 'B'], ['D', 'R'], ['D', 'F'], ['D', 'L'], ['D', 'B'], ['F', 'R'], ['F', 'L'], ['B', 'L'], ['B', 'R']];
 
+
+Cube.prototype.de = Array(12).fill(-1);
+
+Cube.prototype.randomizeSideFaces = (function () {
+    var arePermutationsValid, generateValidRandomOrientation, generateValidRandomPermutation, getNumSwaps, isOrientationValid, randint, randomizeOrientation, result, shuffle;
+    randint = function (min, max) {
+        return min + Math.floor(Math.random() * (max - min + 1));
+    };
+    // Fisher-Yates shuffle adapted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    shuffle = function (array) {
+        var currentIndex, randomIndex, temporaryValue;
+        currentIndex = array.length;
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+            // Pick a remaining element...
+            randomIndex = randint(0, currentIndex - 1);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+    };
+    getNumSwaps = function (arr) {
+        var cur, cycleLength, i, k, numSwaps, ref, seen, x;
+        numSwaps = 0;
+        seen = (function () {
+            var k, ref, results;
+            results = [];
+            for (x = k = 0, ref = arr.length - 1; (0 <= ref ? k <= ref : k >= ref); x = 0 <= ref ? ++k : --k) {
+                results.push(false);
+            }
+            return results;
+        })();
+        while (true) {
+            // We compute the cycle decomposition
+            cur = -1;
+            for (i = k = 0, ref = arr.length - 1; (0 <= ref ? k <= ref : k >= ref); i = 0 <= ref ? ++k : --k) {
+                if (!seen[i]) {
+                    cur = i;
+                    break;
+                }
+            }
+            if (cur === -1) {
+                break;
+            }
+            cycleLength = 0;
+            while (!seen[cur]) {
+                seen[cur] = true;
+                cycleLength++;
+                cur = arr[cur];
+            }
+            // A cycle is equivalent to cycleLength + 1 swaps
+            numSwaps += cycleLength + 1;
+        }
+        return numSwaps;
+    };
+    arePermutationsValid = function (cp, ep) {
+        var numSwaps;
+        numSwaps = getNumSwaps(ep) + getNumSwaps(cp);
+        return numSwaps % 2 === 0;
+    };
+    generateValidRandomPermutation = function (cp, ep) {
+        // Each shuffle only takes around 12 operations and there's a 50%
+        // chance of a valid permutation so it'll finish in very good time
+        // Get undefined subset
+        var epu = ep.slice(8, 12);
+        var cpu = cp.slice(8, 12);
+        shuffle(epu);
+        shuffle(cpu);
+        while (!arePermutationsValid([...cp.slice(0, 8), ...cpu], [...ep.slice(0, 8), ...epu])) {
+            shuffle(epu);
+            shuffle(cpu);
+        }
+        ep = [...ep.slice(0, 8), ...epu];
+        cp = [...cp.slice(0, 8), ...cpu];
+    };
+    randomizeOrientation = function (arr, numOrientations) {
+        var i, k, ori, ref;
+        ori = 0;
+        for (i = k = 0, ref = arr.length - 1; (0 <= ref ? k <= ref : k >= ref); i = 0 <= ref ? ++k : --k) {
+            ori += (arr[i] = randint(0, numOrientations - 1));
+        }
+    };
+    isOrientationValid = function (arr, numOrientations) {
+        return arr.reduce(function (a, b) {
+            return a + b;
+        }) % numOrientations === 0;
+    };
+    generateValidRandomOrientation = function (co, eo) {
+        // There is a 1/2 and 1/3 probably respectively of each of these
+        // succeeding so the probability of them running 10 times before
+        // success is already only 1% and only gets exponentially lower
+        // and each generation is only in the 10s of operations which is nothing
+        randomizeOrientation(co, 3);
+        while (!isOrientationValid(co, 3)) {
+            randomizeOrientation(co, 3);
+        }
+        randomizeOrientation(eo, 2);
+        while (!isOrientationValid(eo, 2)) {
+            randomizeOrientation(eo, 2);
+        }
+    };
+    result = function () {
+        console.log("definedEdges");
+        console.log(this.de);
+        generateValidRandomPermutation(this.cp, this.ep);
+        generateValidRandomOrientation(this.co, this.eo);
+        return this;
+    };
+    return result;
+})();
 
 const RUBIK_COLORS_EXTENDED = Object.assign({}, RUBIK_COLORS, { E: { r: 100, g: 100, b: 100 } }) // add the empty element
 
@@ -122,17 +234,17 @@ export class IncompleteCube {
         return this.reshapeToMatrix(this.backFace.map(rubikToRGB));
     }
 
+
+
     toCompleteCube() {
         var cube = new Cube();
-        var cubeDict = cube.toJSON();
-        // Only need one center to define the needed ones
         var frontCenter = RUBIK_COLORS_A.indexOf(this.frontFace[4]);
-        cubeDict.center = [...cubeDict.center.slice(frontCenter, 6), ...cubeDict.center.slice(0, frontCenter)];
-        if (frontCenter % 2 === 1) { // swap once more if needed for a valid configuration
-            let temp = cubeDict.center[1];
-            cubeDict.center[1] = cubeDict.center[4];
-            cubeDict.center[4] = temp;
-        }
+        // cube.center = [...cube.center.slice(frontCenter, 6), ...cube.center.slice(0, frontCenter)];
+        // if (frontCenter % 2 === 1) { // swap once more if needed for a valid configuration
+        //     let temp = cube.center[1];
+        //     cube.center[1] = cube.center[4];
+        //     cube.center[4] = temp;
+        // }
 
         var definedEdges = [
             this.frontFace[1],
@@ -155,7 +267,7 @@ export class IncompleteCube {
                             cube.eo[i] = (EDGE_COLORS[j][0] === definedEdges[i]) ? 0 : 1;
                             usedEdges[j] = 1;
                             break;
-}
+                        }
                     } else if (i < 11) {
                         // place the reamaining 4
                         cube.ep[i] = j;
@@ -170,6 +282,11 @@ export class IncompleteCube {
                 }
             }
         }
+        cube.randomizeSideFaces();
+        Cube.initSolver();
+        console.log(cube);
+        var movesToSolve1 = cube.solve();
+        cube.move(movesToSolve1);
+        console.log("Is solved? :", cube.isSolved())
     }
 }
-
