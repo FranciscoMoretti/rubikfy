cc11232bc3aa314bc9834e783d3f2665f98aaf63
Francisco Moretti <franciscoemoretti@gmail.com>
2020-02-16 19:10:09 -0300

    added 3rd test case

--- src/Rubikfy/cubeQuantization.test.js
+++ src/Rubikfy/cubeQuantization.test.js
@@ -87,6 +87,49 @@ const TC2 = {
         { "color": "F", "cost": Infinity }],
 }
 
+const TC3 = {
+    lastCornerColorCosts: [195075, 34937, 19493, 112094, 30426, Infinity],
+    colorCount: [0, 0, 2, 0, 1, 4],
+    orderedColorCountDictionary: [
+        { "color": "B", "count": 4 },
+        { "color": "F", "count": 2 },
+        { "color": "L", "count": 1 },
+        { "color": "U", "count": 0 },
+        { "color": "R", "count": 0 },
+        { "color": "D", "count": 0 }],
+    orderedCornerCubelets: [
+        ["U", "L", "B"],
+        ["U", "B", "R"],
+        ["D", "R", "B"],
+        ["D", "B", "L"],
+        ["D", "F", "R"],
+        ["U", "R", "F"],
+        ["D", "L", "F"],
+        ["U", "F", "L"]],
+    infinityCostsRemoved: {
+        parityCountOfRemoved: 6, // or 10 -> should be able to handle multiple
+        remainingCornerCubelets: [
+            ["D", "F", "R"],
+            ["U", "R", "F"],
+            ["D", "L", "F"],
+            ["U", "F", "L"]],
+        remainingColorRepetitions: [
+            { "color": "F", "count": 2 },
+            { "color": "L", "count": 1 },
+            { "color": "U", "count": 0 },
+            { "color": "R", "count": 0 },
+            { "color": "D", "count": 0 }]
+    },
+    resultColorIdx: 4,
+    orderedCornerColorCosts: [
+        { "color": "F", "cost": 19493 },
+        { "color": "L", "cost": 30426 },
+        { "color": "R", "cost": 34937 },
+        { "color": "D", "cost": 112094 },
+        { "color": "U", "cost": 195075 },
+        { "color": "B", "cost": Infinity }]
+}
+
 const PCC_TC1 = {
     orderedColorCount: [
         { "color": "R", "count": 1, },
@@ -116,6 +159,20 @@ const PCC_TC3 = {
     resultParity: 0,
 }
 
+const PCC_TC4 = {
+    orderedColorCount: [
+        { "color": "F", "count": 2 },
+        { "color": "L", "count": 1 },
+        { "color": "U", "count": 0 },
+        { "color": "R", "count": 0 },
+        { "color": "D", "count": 0 }],
+    cubelets: [
+        ["U", "R", "F"],
+        ["D", "L", "F"],
+        ["U", "F", "L"]],
+    resultParity: 6,
+}
+
 test("Function toOrderedColorCountDictionary TC1", () => {
     expect(toOrderedColorCountDictionary(TC1.colorCount)).toStrictEqual(TC1.orderedColorCountDictionary);
 });
@@ -124,12 +181,20 @@ test("Function toOrderedColorCountDictionary TC2", () => {
     expect(toOrderedColorCountDictionary(TC2.colorCount)).toStrictEqual(TC2.orderedColorCountDictionary);
 });
 
+test("Function toOrderedColorCountDictionary TC3", () => {
+    expect(toOrderedColorCountDictionary(TC3.colorCount)).toStrictEqual(TC3.orderedColorCountDictionary);
+});
+
 test("Function sortCornerCubeletsByColorCountOrder TC1", () => {
     expect(sortCornerCubeletsByColorCountOrder(TC1.orderedColorCountDictionary)).toStrictEqual(TC1.orderedCornerCubelets);
 });
 
 test("Function sortCornerCubeletsByColorCountOrder TC2", () => {
     expect(sortCornerCubeletsByColorCountOrder(TC2.orderedColorCountDictionary)).toStrictEqual(TC2.orderedCornerCubelets);
+});
+
+test("Function sortCornerCubeletsByColorCountOrder TC3", () => {
+    expect(sortCornerCubeletsByColorCountOrder(TC3.orderedColorCountDictionary)).toStrictEqual(TC3.orderedCornerCubelets);
 });
 
 test("removeInfinityCountingParity TC1", () => {
@@ -142,6 +207,11 @@ test("removeInfinityCountingParity TC2", () => {
     ).toStrictEqual(TC2.infinityCostsRemoved);
 })
 
+test("removeInfinityCountingParity TC3", () => {
+    expect(removeInfinityCountingParity(TC3.orderedCornerCubelets, TC3.orderedColorCountDictionary, TC3.lastCornerColorCosts)
+    ).toStrictEqual(TC3.infinityCostsRemoved);
+})
+
 test("toOrderedColorCostDictionary TC1", () => {
     expect(toOrderedColorCostDictionary(TC1.lastCornerColorCosts)
     ).toStrictEqual(TC1.orderedCornerColorCosts);
@@ -152,25 +222,37 @@ test("toOrderedColorCostDictionary TC2", () => {
     ).toStrictEqual(TC2.orderedCornerColorCosts);
 })
 
-test("parityCountOfCorners TC1", () => {
+test("toOrderedColorCostDictionary TC3", () => {
+    expect(toOrderedColorCostDictionary(TC3.lastCornerColorCosts)
+    ).toStrictEqual(TC3.orderedCornerColorCosts);
+})
+
+test("parityCountOfCorners PCC_TC1", () => {
     expect(parityCountOfCorners(
         PCC_TC1.cubelets,
         PCC_TC1.orderedColorCount)
     ).toBe(PCC_TC1.resultParity);
 })
 
-test("parityCountOfCorners TC2", () => {
+test("parityCountOfCorners PCC_TC2", () => {
     expect(parityCountOfCorners(
         PCC_TC2.cubelets,
         PCC_TC2.orderedColorCount)
     ).toBe(PCC_TC2.resultParity);
 })
 
-test("parityCountOfCorners TC2", () => {
+test("parityCountOfCorners PCC_TC3", () => {
     expect(parityCountOfCorners(
         PCC_TC3.cubelets,
         PCC_TC3.orderedColorCount)
     ).toBe(PCC_TC3.resultParity);
+})
+
+test("parityCountOfCorners PCC_TC4", () => {
+    expect(parityCountOfCorners(
+        PCC_TC4.cubelets,
+        PCC_TC4.orderedColorCount)
+    ).toBe(PCC_TC4.resultParity);
 })
 
 test("Define Last Corner TC1", () => {
@@ -185,4 +267,11 @@ test("Define Last Corner TC2", () => {
     var colorCount = TC2.colorCount;
     var lastCornerColorIndex = lastCornerColorWithOrientationChecked(lastCornerColorCosts, colorCount);
     expect(lastCornerColorIndex).toBe(TC2.resultColorIdx);
+});
+
+test("Define Last Corner TC3", () => {
+    var lastCornerColorCosts = TC3.lastCornerColorCosts;
+    var colorCount = TC3.colorCount;
+    var lastCornerColorIndex = lastCornerColorWithOrientationChecked(lastCornerColorCosts, colorCount);
+    expect(lastCornerColorIndex).toBe(TC3.resultColorIdx);
 });
